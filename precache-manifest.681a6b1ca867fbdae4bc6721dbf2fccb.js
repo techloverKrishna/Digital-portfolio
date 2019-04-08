38e83bf1c9d711db3e8a67154c0412654f6d5493
krishna <krushnabhamare@gmail.com>
2019-04-08 22:39:51 +0530

    Updates

---
+++ precache-manifest.681a6b1ca867fbdae4bc6721dbf2fccb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "99fcd46eb50bdde8a35b",
+    "url": "/Digital-portfolio/static/js/runtime~main.99fcd46e.js"
+  },
+  {
+    "revision": "5273b83830f6ac917043",
+    "url": "/Digital-portfolio/static/js/main.5273b838.chunk.js"
+  },
+  {
+    "revision": "00d770bde042c74147f0",
+    "url": "/Digital-portfolio/static/js/1.00d770bd.chunk.js"
+  },
+  {
+    "revision": "5273b83830f6ac917043",
+    "url": "/Digital-portfolio/static/css/main.d04abe07.chunk.css"
+  },
+  {
+    "revision": "252e5d8a1dc67905f3bcd148e8920a56",
+    "url": "/Digital-portfolio/index.html"
+  }
+];
